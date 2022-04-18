42ab97078c07ebe8a02c7a977e658c532a934284
FcoJesusDiaz <FcoJesus.Diaz@alu.uclm.es>
2022-04-18 13:09:00 +0200

    Removed function compute_smallest_number

--- include/app_init.h
+++ include/app_init.h
@@ -5,4 +5,3 @@
 void distribute_numbers(int rank, int size, int *n_numbers, int *num_available, double buf[]);
 void send_numbers(double *num_array, int n_numbers);
 void print_message(char *string, int rank);
-void compute_smaller(int rank, int n_numbers, double buf[], int num_available);
